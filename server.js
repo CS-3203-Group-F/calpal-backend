f25b5a14f13aaa6e1a7db373f7075969b57043fd
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-04-04 15:38:25 -0500

    Initial Server Setup

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Test route
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
-const port = process.env.PORT || 3000;
+// Set up routes
 
+// Check if the server is running
 app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
