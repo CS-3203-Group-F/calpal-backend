f2aafc21cdd3a16021bc09b7c8eb1e3986d28d00
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-04-10 17:53:03 -0500

    removed id since it is serial

--- models/users.js
+++ models/users.js
@@ -1,21 +1,22 @@
-class User { //Sprint 1 Issue #4 User Authentication
-    constructor(id, email, password, firstName, lastName) {
-      this.id = id;
-      this.email = email;
-      this.password = password;
-      this.firstName = firstName;
-      this.lastName = lastName;
-    }
-
-    static fromJSON(jsonObj) { //Sprint 1 Issue #4 User Authentication
-      //console.log(jsonObj);
-      return new User(jsonObj.id, jsonObj.email, jsonObj.passwordhash, jsonObj.first_name, jsonObj.last_name);
-    }
-    
-
-  
-    
+class User {
+  //Sprint 1 Issue #4 User Authentication
+  constructor(email, password, firstName, lastName) {
+    this.email = email;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
-  
-  module.exports = User;
-  
+
+  static fromJSON(jsonObj) {
+    //Sprint 1 Issue #4 User Authentication
+    //console.log(jsonObj);
+    return new User(
+      jsonObj.email,
+      jsonObj.passwordhash,
+      jsonObj.first_name,
+      jsonObj.last_name
+    );
+  }
+}
+
+module.exports = User;
