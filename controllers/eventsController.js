2bc84746ca87c058875415870bfddb35330c754b
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-04-10 23:06:26 -0500

    Remove console.logs

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -5,7 +5,6 @@ const EventIdsByUserId = async (req, res) => {
   try {
     const userId = req.params.userId;
     const eventIds = await eventsService.getEventIdsByUserId(userId);
-    console.log(eventIds);
     res.json(eventIds);
   } catch (err) {
     res.status(500).send({ message: err.message });
@@ -16,7 +15,6 @@ const EventDetailsById = async (req, res) => {
   try {
     const eventId = req.params.eventId;
     const eventDetails = await eventsService.getEventDetailsById(eventId);
-    console.log(eventDetails);
     res.json(eventDetails);
   } catch (err) {
     res.status(500).send({ message: err.message });
