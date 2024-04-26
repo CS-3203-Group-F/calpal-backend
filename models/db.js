8607d01914ad34bb74cc4d6af45949588d04a6ad
Caleb <113398593+AcidicMoss@users.noreply.github.com>
2024-04-26 10:53:52 -0500

    set up sequelize

--- models/db.js
+++ models/db.js
@@ -1,16 +1,23 @@
-const { Pool } = require("pg");
+// config/database.js
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-// Load environment variables
-require("dotenv").config();
+// Create a Sequelize instance
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE, // database
+  process.env.DB_USER, // username
+  process.env.DB_PASSWORD, // password
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres', // Assuming you're using PostgreSQL
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // You may need to set this to true in production depending on your database configuration
+      }
+    }
+  }
+);
 
-// Create a new pool instance
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-});
-
-// Export the pool for use in other modules
-module.exports = pool;
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
+
+
 const express = require("express");
 const cors = require("cors");
-const db = require("./models/db");
+const db = require("./models/db"); // Assuming you have an index.js file in your models directory that imports all models
 const eventsRoutes = require("./routes/eventsRoutes");
 
 const app = express();
@@ -15,24 +17,20 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
-// Function to test the database connection
-const testDbConnection = async () => {
-  try {
-    const res = await db.query("SELECT NOW()");
-    console.log(`Database connection successful: ${res.rows[0].now}`);
-  } catch (err) {
-    console.error(`Failed to connect to the database: ${err.message}`);
-    process.exit(1); // Exit the application if there is a connection error
-  }
-};
-
-// Call the function to test the DB connection
-testDbConnection();
+// Test the database connection
+db.sequelize.authenticate()
+  .then(() => {
+    console.log('Database connection successful');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+    process.exit(1);
+  });
 
 // Set up routes
 app.use("/", eventsRoutes);
 
 // Check if the server is running
 app.listen(port, () => {
-  console.log("Server is running on port 3001");
+  console.log(`Server is running on port ${port}`);
 });
