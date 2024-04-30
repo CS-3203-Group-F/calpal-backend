8f8b090614a756cfb08dcc266f4003ac069457ea
Caleb <113398593+AcidicMoss@users.noreply.github.com>
2024-04-29 23:42:04 -0500

    define association from group objects to users and events

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 const eventModel = require("./events");
 const userModel = require("./users");
-
+const groupsModel = require("./groups");
 // Load environment variables
 require("dotenv").config();
 
@@ -27,18 +27,34 @@ const sequelize = new Sequelize(
 const db = {};
 db.Event = eventModel(sequelize);
 db.User = userModel(sequelize);
+db.Groups =groupsModel(sequelize);
 
 //Define associations
+
+//Users to ...
 db.User.belongsToMany(db.Event, {
   through: "UsersEvents",
   foreignKey: "user_id",
   otherKey: "event_id",
 });
+db.User.belongsToMany(db.Groups, {
+  through: "UsersGroups",
+  foreignKey: "user_id",
+  otherKey: "group_id",
+});
+
+// Events to ...
 db.Event.belongsToMany(db.User, {
   through: "UsersEvents",
   foreignKey: "event_id",
   otherKey: "user_id",
 });
+db.Event.belongsTo(db.Groups, {
+  through: "EventsGroups",
+  foreignKey: "event_id",
+  otherKey: "group_id",
+});
+
 
 sequelize.sync({ force: true });
 // Export the Sequelize instance for use in other modules
