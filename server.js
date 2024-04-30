75f850e1d6b09d1b7aa5c0acc1ba6ecf28d674a2
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-04-30 15:19:39 -0500

    Removed commented out code.

--- server.js
+++ server.js
@@ -38,24 +38,6 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
-// Define an async function
-const startDB = async () => {
-  try {
-    await db.sequalize.authenticate();
-    console.log("Database connection successful");
-  } catch (err) {
-    console.error("Unable to connect to the database:", err);
-    process.exit(1);
-  }
-
-  await db.sequelize.sync({ force: false });
-};
-
-// startDB();
-// setTimeout(() => {
-//   console.log("10 seconds have passed.");
-// }, 10000); // 10000 milliseconds = 10 seconds
-
 // Set up routes
 app.use("/", eventsRoutes);
 app.use("/", authRoutes);
