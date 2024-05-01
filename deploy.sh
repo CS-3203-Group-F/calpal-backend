9de33f898338ae0c000e9da2f9f571e3aa7c7040
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-05-01 01:03:12 -0500

    deploy script manual, hook not working

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-BRANCH=$1
-if [ "$BRANCH" == "refs/heads/main" ]; then
-    cd calpal-backend
-    git pull origin main
-    npm install
-    pm2 restart all
-else
-    echo "Not main branch. Not deploying."
-fi
+
+cd calpal-backend
+git pull origin main
+npm install
+pm2 restart all
+
