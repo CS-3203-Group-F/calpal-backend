47a77e91cf798e81adaef8a07b38faf06b9dafc5
Caleb <113398593+AcidicMoss@users.noreply.github.com>
2024-05-02 12:36:53 -0500

    comment services

--- services/authService.js
+++ services/authService.js
@@ -2,9 +2,13 @@
 const bcrypt = require("bcrypt");
 const db = require("../models/db");
 
+
+// Create a new user in the database with the provided email, password, first name, and last name
 const createUser = async (email, password, firstName, lastName) => {
   try {
+    // Hash the password using bcrypt
     const hashedPassword = await bcrypt.hash(password, 10);
+    // Create a new user in the database using sqlize's create method with the provided data
     const user = await db.User.create({
       email: email,
       passwordhash: hashedPassword,
@@ -18,20 +22,28 @@ const createUser = async (email, password, firstName, lastName) => {
   }
 };
 
+// Verify the user with the provided email and password
 const verifyUser = async (email, password) => {
   try {
+    // Find the user in the database with the provided email using Sequelize's findOne method
     const user = await db.User.findOne({ where: { email: email } });
+    // If the user exists and the password matches, return the user 
     if (user && (await user.comparePassword(password))) {
       return user; // Return the Sequelize user object
     } else {
+      // if the user does not exist or the password does not match, throw an error
       throw new Error("Invalid credentials");
     }
   } catch (error) {
+    // If an error occurs, log the error and throw a new error
     throw new Error("Error verifying user");
   }
 };
 
+
+// Add a comparePassword method to the User model to compare the provided password with the user's password hash
 db.User.prototype.comparePassword = function (password) {
+  // Use bcrypt's compare method to compare the provided password with the user's password hash
   return bcrypt.compare(password, this.passwordhash); // ensure 'passwordhash' matches the database column
 };
 
--- services/eventsService.js
+++ services/eventsService.js
@@ -1,7 +1,10 @@
 const db = require("../models/db");
 
+// Create a new event in the database with the provided event data
 const createEvent = async (eventData) => {
   try {
+    // Create a new event in the database using Sequelize's create method with the provided data
+    // store the created event in a variable
     const event = await db.Event.create({
       title: eventData.title,
       description: eventData.description,
@@ -12,34 +15,43 @@ const createEvent = async (eventData) => {
       color: eventData.color,
       location: eventData.location,
     });
-    // Create an entry in the UsersEvents table
+    // Create associations between the event and the user and group
     await event.addUser(eventData.user_id);
     await event.addGroup(eventData.group_id);
+    // Return the Sequelize event object as a JSON object
     return event.toJSON();
   } catch (err) {
+    // If an error occurs, log the error and throw a new error
     throw new Error(`Unable to create event: ${err.message}`);
   }
 };
 
+// Edit an existing event in the database with the provided event ID and event data
 const editEventById = async (eventId, eventData) => {
   try {
+    // Find the event in the database with the provided ID using Sequelize's findByPk method
     const event = await db.Event.findByPk(eventId);
     if (!event) {
+      // If the event does not exist, throw an error
       throw new Error(`Event with ID ${eventId} not found`);
     }
-
+    // Update the event with the provided data using Sequelize's update method
     const updatedEvent = await event.update(eventData);
 
+    // Return the updated event as a JSON object
     return event.toJSON();
   } catch (err) {
+    // If an error occurs, log the error and throw a new error
     throw new Error(`Unable to edit event with ID ${eventId}: ${err.message}`);
   }
 };
 
+// Get event IDs associated with a user ID
 const getEventIdsByUserId = async (userId) => {
   try {
     // Fetch the user with associated events
     const userWithEvents = await db.User.findByPk(userId, {
+      // Include the Event model and fetch only the event_id
       include: [
         {
           model: db.Event,
@@ -51,6 +63,7 @@ const getEventIdsByUserId = async (userId) => {
       ],
     });
 
+    // If the user is not found, throw an error
     if (!userWithEvents) {
       throw new Error("User not found");
     }
@@ -59,22 +72,27 @@ const getEventIdsByUserId = async (userId) => {
     const eventIds = userWithEvents.Events.map(
       (event) => event.UsersEvents.event_id
     );
+    // Return the event IDs
     return eventIds;
   } catch (err) {
     console.log(err);
+    // If an error occurs, log the error and throw a new error
     throw new Error(
       `Unable to retrieve event IDs for user ${userId}: ${err.message}`
     );
   }
 };
 
+// Get event details by event ID
 const getEventDetailsById = async (eventId) => {
   try {
+    // Find the event in the database with the provided ID using Sequelize's findOne method
     const event = await db.Event.findOne({
       where: { event_id: eventId }, // ensure the primary key on your Event model is 'id' or adjust accordingly
     });
     return event ? event.toJSON() : null; // Convert Sequelize instance to plain object
   } catch (err) {
+    // If an error occurs, log the error and throw a new error
     throw new Error(
       `Unable to retrieve event details for event ${eventId}: ${err.message}`
     );
