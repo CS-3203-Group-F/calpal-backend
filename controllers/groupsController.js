dd691648da2bdf6eed2ba58dab394363a643b615
Caleb <113398593+AcidicMoss@users.noreply.github.com>
2024-05-02 11:48:25 -0500

    comment groups controller

--- controllers/groupsController.js
+++ controllers/groupsController.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const groupsService = require("../services/groupsService");
 
+// controller for getting groups by user ID takes a request and response object as arguments
 const getGroupsByUserId = async (req, res) => {
   try {
+    // destructure the user ID from the request parameters
     const userId = req.params.userId;
+    // call the getGroupIdsByUserId method from the groupsService with the user ID as an argument
     const groups = await groupsService.getGroupIdsByUserId(userId);
     res.json(groups);
   } catch (err) {
@@ -11,9 +14,12 @@ const getGroupsByUserId = async (req, res) => {
   }
 };
 
+// controller for getting group details by ID takes a request and response object as arguments
 const getGroupDetailsById = async (req, res) => {
   try {
+    // destructure the group ID from the request parameters
     const groupId = req.params.groupId;
+    // call the getGroupDetailsById method from the groupsService with the group ID as an argument
     const groupDetails = await groupsService.getGroupDetailsById(groupId);
     res.json(groupDetails);
   } catch (err) {
@@ -21,9 +27,12 @@ const getGroupDetailsById = async (req, res) => {
   }
 };
 
+// controller for getting group members takes a request and response object as arguments
 const getGroupMembers = async (req, res) => {
   try {
+    // destructure the group ID from the request parameters
     const groupId = req.params.groupId;
+    // call the getGroupMembers method from the groupsService with the group ID as an argument
     const groupMembers = await groupsService.getGroupMembers(groupId);
     res.json(groupMembers);
   } catch (err) {
@@ -31,9 +40,12 @@ const getGroupMembers = async (req, res) => {
   }
 };
 
+// controller for creating a new group takes a request and response object as arguments
 const CreateGroup = async (req, res) => {
   try {
+    // destructure the group data from the request body
     const groupData = req.body;
+    // call the createGroup method from the groupsService with the group data as an argument
     const newGroup = await groupsService.createGroup(groupData);
     res.json(newGroup);
   } catch (err) {
